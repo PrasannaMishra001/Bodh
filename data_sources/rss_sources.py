2c0c2c4cb2585106a8ca388974210aee30dbad0d
Leonidas <mishra.prasanna838@gmail.com>
2025-12-09 17:00:58 +0530

    Switch to Wikipedia HTML scraping for history

--- data_sources/rss_sources.py
+++ data_sources/rss_sources.py
@@ -1,5 +1,4 @@
 RSS_FEEDS = [
-    # You can tune or extend this list. Some examples:
     "https://www.thehindu.com/news/national/feeder/default.rss",
     "https://www.indiatoday.in/rss/1206534", 
     "https://www.pib.gov.in/PressReleaseTaxonomy.aspx?TaxonomyId=2&LanguageId=1",  
